1c61a8c3725fe3d9d21836e4e108452fe142202f
jlika <similika25@gmail.com>
2024-06-13 21:25:14 +0200

    initial commit

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const themeToggle = document.getElementById('theme-toggle');
-    const themeIcon = document.getElementById('theme-icon');
-    const currentTheme = localStorage.getItem('theme') || 'light';
+document.addEventListener('DOMContentLoaded', () => {
+    const toggleSwitch = document.getElementById('theme-toggle');
+    const currentTheme = localStorage.getItem('theme');
 
-    if (currentTheme === 'dark') {
-        document.documentElement.setAttribute('data-theme', 'dark');
-        themeIcon.setAttribute('name', 'sunny-outline');
+    if (currentTheme) {
+        document.body.classList.add(currentTheme);
+        if (currentTheme === 'dark-mode') {
+            toggleSwitch.checked = true;
+        }
     }
 
-    themeToggle.addEventListener('click', function() {
-        const theme = document.documentElement.getAttribute('data-theme');
-        if (theme === 'dark') {
-            document.documentElement.setAttribute('data-theme', 'light');
-            themeIcon.setAttribute('name', 'moon-outline');
-            localStorage.setItem('theme', 'light');
+    toggleSwitch.addEventListener('change', () => {
+        if (toggleSwitch.checked) {
+            document.body.classList.add('dark-mode');
+            localStorage.setItem('theme', 'dark-mode');
         } else {
-            document.documentElement.setAttribute('data-theme', 'dark');
-            themeIcon.setAttribute('name', 'sunny-outline');
-            localStorage.setItem('theme', 'dark');
+            document.body.classList.remove('dark-mode');
+            localStorage.setItem('theme', 'light-mode');
         }
     });
 });
